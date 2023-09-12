0f29e15051cb84384ca24d36c218c90e8d8e53b8
Sarath Kumar R S <sarath.talrop@gmail.com>
2023-09-12 23:36:31 +0530

    static files other files are configured

--- eventmanagementproject/settings.py
+++ eventmanagementproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'eventmanagementproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://eventdbefx_user:egxrwmz187b27kysoYBYOkOxO2w3ShbV@dpg-ck0a93b6fquc739e8df0-a.oregon-postgres.render.com/eventdbefx')
+    'default': dj_database_url.config(default='postgres://eventdbefx_user:egxrwmz187b27kysoYBYOkOxO2w3ShbV@dpg-ck0a93b6fquc739e8df0-a/eventdbefx')
 }
 
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
